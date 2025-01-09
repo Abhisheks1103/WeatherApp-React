8906c9714e9815ab1194d5530b824a549682422b
Abhisheks1103 <162470389+Abhisheks1103@users.noreply.github.com>
2025-01-09 22:26:16 +0530

    weather-app-implemented

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "d9db0a057f9dd527c76902340172a299",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
